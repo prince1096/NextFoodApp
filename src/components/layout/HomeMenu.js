8bd7fafd15c1a46e8a7e99400fd3f951dfad1a0a
prince1096 <princeraj10121996@gmail.com>
2024-06-24 19:46:38 +0530

    MenuItems

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 
@@ -6,45 +8,83 @@ import Sallad2 from "@/assets/sallad2.png";
 import MenuItem from "../menu/MenuItem";
 import SectionHeaders from "./SectionHeaders";
 
-const HomeMenu = () => {
+// const HomeMenu = () => {
+//   return (
+//     <div>
+//       <section className="">
+//         <div className="absolute h-full left-0 right-0 w-full justify-start">
+//           <div className="absolute left-0 -top-[70px] text-left -z-10">
+//             <Image src={Sallad1} width={109} height={189} alt={"sallad"} />
+//           </div>
+//           <div className="absolute -top-[100px] right-0 -z-10">
+//             <Image src={Sallad2} width={107} height={195} alt={"sallad"} />
+//           </div>
+//         </div>
+
+//         <div className="text-center mb-4">
+//           <SectionHeaders subHeader={"Check out"} mainHeader={"Menu"} />
+//         </div>
+
+//         <div className="grid grid-cols-3 gap-8">
+//           <MenuItem />
+//           <MenuItem />
+//           <MenuItem />
+//           <MenuItem />
+//           <MenuItem />
+//           <MenuItem />
+//         </div>
+
+//         <div className="text-center mb-4">
+//           {/* <SectionHeaders
+//             subHeader={"check out"}
+//             mainHeader={"Our Best Sellers"}
+//           />  */}
+//         </div>
+//         <div className="grid sm:grid-cols-3 gap-4">
+//           {/* {bestSellers?.length > 0 &&
+//             bestSellers.map((item) => <MenuItem key={item._id} {...item} />)} */}
+//         </div>
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default HomeMenu;
+
+// import SectionHeaders from "@/components/layout/SectionHeaders";
+// import MenuItem from "@/components/menu/MenuItem";
+// import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function HomeMenu() {
+  const [bestSellers, setBestSellers] = useState([]);
+  useEffect(() => {
+    fetch("/api/menuItems").then((res) => {
+      res.json().then((menuItems) => {
+        setBestSellers(menuItems.slice(-3));
+      });
+    });
+  }, []);
   return (
-    <div>
-      <section className="">
-        <div className="absolute h-full left-0 right-0 w-full justify-start">
-          <div className="absolute left-0 -top-[70px] text-left -z-10">
-            <Image src={Sallad1} width={109} height={189} alt={"sallad"} />
-          </div>
-          <div className="absolute -top-[100px] right-0 -z-10">
-            <Image src={Sallad2} width={107} height={195} alt={"sallad"} />
-          </div>
+    <section className="">
+      <div className="absolute left-0 right-0 w-full justify-start">
+        <div className="absolute left-0 -top-[70px] text-left -z-10">
+          <Image src={Sallad1} width={109} height={189} alt={"sallad"} />
         </div>
-
-        <div className="text-center mb-4">
-          <SectionHeaders subHeader={"Check out"} mainHeader={"Menu"} />
+        <div className="absolute -top-[100px] right-0 -z-10">
+          <Image src={Sallad2} width={107} height={195} alt={"sallad"} />
         </div>
-
-        <div className="grid grid-cols-3 gap-8">
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-        </div>
-
-        <div className="text-center mb-4">
-          {/* <SectionHeaders
-            subHeader={"check out"}
-            mainHeader={"Our Best Sellers"}
-          />  */}
-        </div>
-        <div className="grid sm:grid-cols-3 gap-4">
-          {/* {bestSellers?.length > 0 &&
-            bestSellers.map((item) => <MenuItem key={item._id} {...item} />)} */}
-        </div>
-      </section>
-    </div>
+      </div>
+      <div className="text-center mb-4">
+        <SectionHeaders
+          subHeader={"check out"}
+          mainHeader={"Our Best Sellers"}
+        />
+      </div>
+      <div className="grid sm:grid-cols-3 gap-4">
+        {bestSellers?.length > 0 &&
+          bestSellers.map((item) => <MenuItem key={item._id} {...item} />)}
+      </div>
+    </section>
   );
-};
-
-export default HomeMenu;
+}
